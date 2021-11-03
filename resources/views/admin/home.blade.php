1de02419e2dc8474602e4b8d4eb98303d4c4aa8e
Hafiz <hafizhadi581@gmail.com>
2021-11-03 09:31:43 +0800

    Middleware added

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Admin Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+            <br>
+            <div class="card">
+                <div class="card-header">{{ __('Component') }}</div>
+
+                <div class="card-body">
+                    <a href="{{ route('component:index') }}" type="button" class="btn btn-primary">Index</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/admin/home', [HomeController::class, 'admin'])->name('admin:home')->middleware('is.admin');//middleware 
 
 //feature component
 
